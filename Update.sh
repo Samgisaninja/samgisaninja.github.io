8b2343ac57386530f9fc48955c1b9713cfe4999e
Sam Gardner <stgardner4@att.net>
2019-07-10 11:10:03 -0500

    update deps

--- Update.sh
+++ Update.sh
@@ -2,15 +2,7 @@
 rm Packages
 dpkg-scanpackages -m ./debs > Packages
 rm -f Packages.bz2
+./UpdateDepiction
 cp Packages Packages2
 bzip2 Packages
 mv Packages2 Packages
-
-#!/bin/bash
-for file in ./debs/*
-do
-  /usr/local/bin/dpkg-deb -I "$file"
-  echo Filename: $file
-  echo "AUTOREPO_NEXTPACKAGE"
-  sleep 0.1
-done
