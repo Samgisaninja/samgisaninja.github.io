cc1d8ae81902509a614b2cf3075c4272f2075dff
Sam Gardner <stgardner4@att.net>
2019-07-10 10:56:54 -0500

    add depictions

--- GetPackageInfo.sh
+++ GetPackageInfo.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-rm Packages
-dpkg-scanpackages -m ./debs > Packages
-rm -f Packages.bz2
-cp Packages Packages2
-bzip2 Packages
-mv Packages2 Packages
-
-#!/bin/bash
 for file in ./debs/*
 do
   /usr/local/bin/dpkg-deb -I "$file"
@@ -14,3 +6,4 @@ do
   echo "AUTOREPO_NEXTPACKAGE"
   sleep 0.1
 done
+sleep 10
