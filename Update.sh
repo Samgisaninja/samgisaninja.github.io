59eb1d8b4d26ec5e04a167bb77bb3e27b8b30afc
Sam Gardner <stgardner4@att.net>
2019-11-12 01:01:06 -0600

    twc-z

--- Update.sh
+++ Update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm Packages
-dpkg-scanpackages -m ./debs > Packages
 rm -f Packages.bz2
+dpkg-scanpackages -m ./debs > Packages
 ./UpdateDepiction
 cp Packages Packages2
 bzip2 Packages
